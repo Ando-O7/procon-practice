967d4afce6e55fa3fe0eea2d012c856bd88df95f
ando07 <ando.masahito@gmail.com>
2019-12-21 23:28:10 +0900

    [modify]restore namespace

--- ants/c++/ants.cpp
+++ ants/c++/ants.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 
+using namespace std;
 void slove();
 // ===== ↓↓↓input↓↓↓ ===== //
 const int MAX_N = 3;
@@ -16,12 +17,12 @@ void slove() {
 	// min time
 	int min_time = 0;
 	for (int i = 0; i < n; i++) {
-		min_time = max(min_time, min(x[i], L - x[i]));
+		min_time = std::max(min_time, std::min(x[i], L - x[i]));
 	}
 	// max time
 	int max_time = 0;
 	for (int i = 0; i < n; i++) {
-		max_time = max(max_time, max(x[i], L - x[i]));
+		max_time = std::max(max_time, std::max(x[i], L - x[i]));
 	}
 	
 	printf("%d %d\n", min_time, max_time);
