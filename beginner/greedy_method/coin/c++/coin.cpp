cea5a806d468188d8c81e0c53650bb24c11b8603
ando07 <ando.masahito@gmail.com>
2020-01-05 21:32:44 +0900

    [modify]Array storage

--- beginner/greedy_method/coin/c++/coin.cpp
+++ beginner/greedy_method/coin/c++/coin.cpp
@@ -3,13 +3,12 @@ using namespace std;
 
 // ===== ↓↓↓input↓↓↓ ===== //
 const int V[6] = {1, 5, 10, 50, 100, 500};
-int C[6];
+int C[6] = {3, 2, 1, 3, 0, 1};
 int A = 620;
 // ===== ↑↑↑input↑↑↑ ===== //
 
 int main(int argc, char *argv[]) {
 	int ans = 0;
-    C[0] = 3, C[1] = 2,  C[2] = 1,  C[3] = 3, C[4] = 0, C[5] = 2;
 
     for (int i = 5; i >= 0; i--) {
         int t = min(A / V[i], C[i]);
