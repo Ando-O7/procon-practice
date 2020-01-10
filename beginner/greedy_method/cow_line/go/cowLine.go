cd8c5b0c5ec55f01f553d3842023ff337435870e
ando07 <ando.masahito@gmail.com>
2020-01-10 23:54:58 +0900

    [add]cow line code go

---
+++ beginner/greedy_method/cow_line/go/cowLine.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// ===== ↓↓↓input↓↓↓ ===== //
+const MAX_N = 6
+const N = MAX_N
+
+var S = [MAX_N]string{"A", "C", "D", "B", "C", "B"}
+
+// ===== ↑↑↑input↑↑↑ ===== //
+
+func main() {
+	a := 0
+	b := N - 1
+
+	for a <= b {
+		left := false
+		for i := 0; a+i <= b; i++ {
+			if S[a+i] < S[b-i] {
+				left = true
+				break
+			} else if S[a+i] > S[b-i] {
+				left = false
+				break
+			}
+		}
+
+		if left {
+			a++
+			fmt.Print(S[a])
+		} else {
+			b--
+			fmt.Print(S[b])
+		}
+	}
+}
